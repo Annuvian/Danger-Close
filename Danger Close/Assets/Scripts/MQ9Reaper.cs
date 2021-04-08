56b20a6f0ba528a3c29ea8d1c1ae6ad1097ab466
Robbie Frank <r.frank117@gmail.com>
2021-04-08 14:47:14 -0400

    Save point before removing multiplayer

---
+++ Danger Close/Assets/Scripts/MQ9Reaper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MQ9Reaper : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
