fd8b2b1f4d2ebcc56d3d3bd3caa87fec369f6230
Robbie Frank <r.frank117@gmail.com>
2021-04-08 16:10:45 -0400

    Player can now zoom in and out with the scroll wheel and the cursor is locked to the center of the screen and not displayed

--- Danger Close/Assets/Scripts/Player.cs
+++ Danger Close/Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     // Fields
     [SerializeField] float lookSensitivity = 10f;
+    float minimumYTilt = -17f;
+    float maximumYTilt = 71f;
+    [SerializeField] float zoomSpeed = 5f;
+    [SerializeField] float maximumFieldOfView = 70f;
+    [SerializeField] float minimumFieldOfView = 0f;
     [SerializeField] float currentYTilt;
     [SerializeField] float currentXTilt;
 
@@ -15,13 +20,15 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         LookAround();
+        AdjustZoom();
     }
 
     void LookAround()
@@ -30,8 +37,23 @@ public class Player : MonoBehaviour
         float y = Input.GetAxis("Mouse Y");
         currentYTilt -= y * lookSensitivity;
         currentXTilt += x * lookSensitivity;
-        currentYTilt = Mathf.Clamp(currentYTilt, -17, 71);
+        currentYTilt = Mathf.Clamp(currentYTilt, minimumYTilt, maximumYTilt);
         //currentXTilt = Mathf.Clamp(currentXTilt, -90, 90);
         cameraArray.transform.localEulerAngles = new Vector3(currentYTilt, currentXTilt, 0);
+    }
+
+    void AdjustZoom()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        if (zoom > 0f && Camera.main.fieldOfView > minimumFieldOfView)
+        {
+            Camera.main.fieldOfView -= zoom;
+            Debug.Log(zoom);
+        }
+        else if (zoom < 0f && Camera.main.fieldOfView < maximumFieldOfView)
+        {
+            Camera.main.fieldOfView -= zoom;
+            Debug.Log(zoom);
+        }
     }
 }
